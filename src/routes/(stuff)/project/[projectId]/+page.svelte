2756f3f50f3a810c5c6479e718d143a95d3512f3
Neville Brem <contact@nevillebrem.com>
2025-03-10 21:52:08 +0100

    color; project page

--- src/routes/(stuff)/project/[projectId]/+page.svelte
+++ src/routes/(stuff)/project/[projectId]/+page.svelte
@@ -6,26 +6,30 @@
 	let { project } = $derived(data);
 </script>
 
-<h1 class="mb-2 text-4xl font-bold">{project.title}</h1>
-{#if project.date}
-	<p>{project.date}</p>
-{:else}
-	<p class="text-gray-600 italic">No date set</p>
-{/if}
-
-<p
-	class={cn(
-		'w-fit rounded-full px-2 py-1 capitalize',
-		project.status === 'To Do'
-			? 'bg-todo/50'
-			: project.status === 'Re Record'
-				? 'bg-rerecord/50'
-				: project.status === 'In Progress'
-					? 'bg-inprogress/50'
-					: project.status === 'Done'
-						? 'bg-done/50'
-						: 'bg-scrap/50'
-	)}
->
-	{project.status}
+<h1 class="mb-2 text-4xl font-bold">
+	<span class="text-muted font-semibold italic">Project:</span>
+	{project.title}
+</h1>
+<p class="flex items-center gap-2">
+	{#if project.date}
+		<span>{project.date}</span>
+	{:else}
+		<span class="text-muted italic">No date set</span>
+	{/if}
+	<span
+		class={cn(
+			'w-fit rounded-full px-2 py-1 capitalize',
+			project.status === 'To Do'
+				? 'bg-todo/50'
+				: project.status === 'Re Record'
+					? 'bg-rerecord/50'
+					: project.status === 'In Progress'
+						? 'bg-inprogress/50'
+						: project.status === 'Done'
+							? 'bg-done/50'
+							: 'bg-scrap/50'
+		)}
+	>
+		{project.status}
+	</span>
 </p>
