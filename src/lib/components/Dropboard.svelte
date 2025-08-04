535bdb28a421b20448d5e7aafddd5d8c7399d4ae
Neville Brem <contact@nevillebrem.com>
2025-08-04 11:05:54 +0200

    improved loading states

--- src/lib/components/Dropboard.svelte
+++ src/lib/components/Dropboard.svelte
@@ -5,7 +5,7 @@
 	import { cn, type STATUS_VALUES } from '$lib/utils';
 	import { droppable, type DragDropState } from '@thisux/sveltednd';
 	import ProjectCard from './ProjectCard.svelte';
-	import { Plus } from 'lucide-svelte';
+	import { Loader2, Plus } from 'lucide-svelte';
 
 	let { statusValue }: { statusValue: (typeof STATUS_VALUES)[number] } = $props();
 
@@ -62,59 +62,63 @@
 		callbacks: { onDrop: handleDrop }
 	}}
 >
-	<div class="mb-4 flex items-center justify-between">
-		<h2 class="font-semibold capitalize">
-			{statusValue}
-		</h2>
-		<span class="px-2.5 py-0.5 text-sm">
-			<svelte:boundary>
-				{#snippet pending()}
-					0
-				{/snippet}
-				{(await getProjects()).filter((p) => p.status === statusValue).length}
-			</svelte:boundary>
-		</span>
-	</div>
+	<svelte:boundary>
+		{#snippet pending()}
+			<div class="flex justify-between">
+				{@render statusName(statusValue)}
+				<Loader2 class="animate-spin" />
+			</div>
+		{/snippet}
+		<div class="mb-4 flex items-center justify-between">
+			{@render statusName(statusValue)}
+			<span class="px-2.5 py-0.5 text-sm">
+				<svelte:boundary>
+					{(await getProjects()).filter((p) => p.status === statusValue).length}
+				</svelte:boundary>
+			</span>
+		</div>
 
-	<div class="space-y-3">
-		<svelte:boundary>
-			{#snippet pending()}
-				<p class="font-mono italic">Loading "{statusValue}" projects...</p>
-			{/snippet}
+		<div class="space-y-3">
 			{#each (await getProjects()).filter((p) => p.status === statusValue) as prj (prj.id)}
 				<ProjectCard project={prj} status={statusValue} />
 			{/each}
-		</svelte:boundary>
-		{#if statusValue === 'To Do'}
-			{#if newItem}
-				<form
-					{...newProject.enhance(async ({ submit }) => {
-						try {
-							submit().updates(getProjects());
-							newItem = false;
-						} catch (e) {
-							toast.error(e as string);
-						}
-					})}
-				>
-					<input
-						bind:this={newInput}
-						class="bg-dark/10 rounded-card w-full border-2 p-4 text-xl font-bold"
-						type="text"
-						name="title"
-						placeholder="New Project"
-					/>
-				</form>
-			{/if}
-			<button
-				onclick={() => {
-					newItem = true;
-					setTimeout(() => {
-						newInput.focus();
-					}, 20);
-				}}
-				class="bg-dark/10 rounded-input flex w-full items-center justify-center gap-2 border-2 p-2 text-lg font-bold"
-				><Plus size={20} />New Item</button
-			>{/if}
-	</div>
+			{#if statusValue === 'To Do'}
+				{#if newItem}
+					<form
+						{...newProject.enhance(async ({ submit }) => {
+							try {
+								submit().updates(getProjects());
+								newItem = false;
+							} catch (e) {
+								toast.error(e as string);
+							}
+						})}
+					>
+						<input
+							bind:this={newInput}
+							class="bg-dark/10 rounded-card w-full border-2 p-4 text-xl font-bold"
+							type="text"
+							name="title"
+							placeholder="New Project"
+						/>
+					</form>
+				{/if}
+				<button
+					onclick={() => {
+						newItem = true;
+						setTimeout(() => {
+							newInput.focus();
+						}, 20);
+					}}
+					class="bg-dark/10 rounded-input flex w-full items-center justify-center gap-2 border-2 p-2 text-lg font-bold"
+					><Plus size={20} />New Item</button
+				>{/if}
+		</div>
+	</svelte:boundary>
 </div>
+
+{#snippet statusName(statusValue: string)}
+	<h2 class="font-semibold capitalize">
+		{statusValue}
+	</h2>
+{/snippet}
