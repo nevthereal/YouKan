42a27b2b257b8696b706f719d02e62ccfcaae4ac
Neville Brem <contact@nevillebrem.com>
2025-08-13 19:05:14 +0200

    add shad

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -19,3 +19,10 @@ export function prettyDate(date: Date, format: 'long' | 'medium') {
 }
 
 export const STATUS_VALUES = ['To Do', 'Re Record', 'In Progress', 'Done', 'Scrap'] as const;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type WithoutChild<T> = T extends { child?: any } ? Omit<T, 'child'> : T;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type WithoutChildren<T> = T extends { children?: any } ? Omit<T, 'children'> : T;
+export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>;
+export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & { ref?: U | null };
